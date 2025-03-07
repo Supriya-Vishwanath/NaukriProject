bd7ba43ccdd71714c69337137368d5ef7fa28246
supriya-vishwanath <supriya.vishwavani@gmail.com>
2025-03-08 00:46:12 +0530

    Initial commit

--- src/test/java/stepDefinition/UploadResumeSteps.java
+++ src/test/java/stepDefinition/UploadResumeSteps.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -13,17 +15,17 @@ import io.cucumber.java.en.When;
 
 public class UploadResumeSteps {
  WebDriver driver;
-
 @Given("User is on Naukri login page")
 public void user_is_on_naukri_login_page() {
 	  driver = new ChromeDriver();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
       driver.manage().window().maximize();
       driver.get("https://www.naukri.com/");
 }
 
 @When("User logs in with valid credentials")
 public void user_logs_in_with_valid_credentials() {
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     driver.findElement(By.id("login_Layer")).click();
     driver.findElement(By.xpath("//input[contains(@placeholder,'Email')]")).sendKeys("supriya.v.prof@gmail.com");
     driver.findElement(By.xpath("//input[contains(@placeholder,'pass')]")).sendKeys("Naukri2025$");
@@ -37,13 +39,16 @@ public void user_navigates_to_the_resume_upload_section() {
 
 @When("User uploads the resume file")
 public void user_uploads_the_resume_file() {
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	 WebElement uploadElement = driver.findElement(By.xpath("//input[@type='file' and @id='attachCV']"));
-     uploadElement.sendKeys("C:\\Users\\91849\\Documents\\Suppa 2.0\\GrowSkills\\Supriya_V_Resume.pdf");
+	WebElement uploadElement = driver.findElement(By.xpath("//input[@type='file' and contains(@id,'attachCV')]"));
+    uploadElement.sendKeys("C:\\Users\\91849\\Documents\\Suppa 2.0\\GrowSkills\\Supriya_V_Resume.pdf");
+
+    System.out.println("File path sent to upload field.");
 }
 
 @Then("Resume should be uploaded successfully")
-public void resume_should_be_uploaded_successfully() {
+public void resume_should_be_uploaded_successfully() throws InterruptedException {
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@title='Supriya_V_Resume.pdf']")));
 	 System.out.println("Resume uploaded successfully!");
      driver.quit();
 }
